2f3deda0c2ea74bc1d920fbfb3227e7f6949c3c9
Tonmoy Ahmed Dhroubo <tonmoyahmeddhroubo@gmail.com>
2026-01-13 01:09:26 +0600

    dark theme as default

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -7,7 +7,7 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
 		<NextThemesProvider
 			attribute="class"
 			defaultTheme="dark"
-			enableSystem={true}
+			enableSystem={false}
 		>
 			{children}
 		</NextThemesProvider>
